7031189246d4240a24a71f85c3bfa0205cefdc60
anpol84 <polakovandrej327@mail.ru>
2024-03-31 18:46:44 +0300

    Did 9 homework

--- bot/src/main/java/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/edu/java/bot/controller/BotController.java
@@ -2,6 +2,8 @@ package edu.java.bot.controller;
 
 import edu.java.bot.service.ProcessMessageService;
 import edu.java.bot.serviceDto.LinkUpdateRequest;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BotController {
 
     private final ProcessMessageService processMessageService;
+    Counter messagesProcessed = Metrics.counter("messages.processed");
 
     @PostMapping
     public String sendUpdate(@RequestBody @Valid LinkUpdateRequest linkUpdate) {
         processMessageService.process(linkUpdate);
+        messagesProcessed.increment();
         return "The update has been processed";
     }
 }
